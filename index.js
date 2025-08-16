f7cfccfce24e145106e0b13ddd76d481e0540661
SHIVANSH YADAV <140878194+shiv665@users.noreply.github.com>
2025-08-16 18:58:53 +0530

    Update index.js
    update and slight mistake in cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://shivansh3286.me/", // Replace with your frontend URL
+    origin: "https://shivansh3286.me", // Replace with your frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
