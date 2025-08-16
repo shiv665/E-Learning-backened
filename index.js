763a43a674893a816dfbde585f218a3afcbbcb6f
SHIVANSH YADAV <140878194+shiv665@users.noreply.github.com>
2025-08-16 18:40:15 +0530

    Update index.js
    frontened url been updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://shivansh3286.me/", // Replace with your frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
